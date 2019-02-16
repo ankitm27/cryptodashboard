4cdc54933eaaaf88b3614922ebb28cb851c4440f
ankitm27 <ankit.malhotra@gamechangesns.com>
2019-02-16 13:15:22 +0530

    add app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage.js';
-import styled ,{css} from 'styled-components';
-
+import AppLayout from './AppLayout.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage />
-      </div>
+      </AppLayout>
     );
   }
 }
